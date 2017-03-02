7a97811485df8d9768d4106dea78c1b429bd7e9f
Randolph Roble <roblerm@gmail.com>
2017-03-02 21:38:34 +0800

    sample

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,24 @@
 angular.module('calcApp', [])
-    .controller('CalculatorController', function() {
+    .controller('CalculatorController', function($scope) {
+        $scope.calc = {
+            capital: 100000,
+            buy: {
+                shares: 100,
+                price: 15,
+                gross: 1500,
+                charges: 15,
+                net: 1600
+            },
+            sell: {
+                shares: 100,
+                price: 15,
+                gross: 1500,
+                charges: 15,
+                net: 1600
+            },
+            profit: {
+                percent: 1,
+                value: 100
+            }
+        };
     });
